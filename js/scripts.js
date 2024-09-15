7ab6ba55c9502ab3df65b684e384c10badea365b
Jessica Nakamura <jessinaka21@gmail.com>
2024-09-14 16:31:30 -1000

    added weather API

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,34 @@ carouselButton.addEventListener("click", function () {
     carousel.cycle();
   }
 })
+
+async function fetchWeather() {
+  const apiKey = process.env.OPEN_WEATHER_API_KEY;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=millinocket,me,us&limit=1&appid=${apiKey}&units=imperial`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    displayWeather(data); 
+}  catch (error) {
+    console.error("There was an error!", error);
+  } 
+}
+
+fetchWeather();
+
+function displayWeather(data) {
+  const weatherDiv = document.querySelector("#weather");
+  // icon
+  const image = document.createElement("img");
+  image.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  document.querySelector("#weather-icon").appendChild(image);
+  // temp
+  const temp = document.createElement("p");
+  temp.textContent = `${data.main.temp}°F`;
+  document.querySelector("#weather-temp").appendChild(temp);
+  // description
+  const description = document.createElement("p");
+  description.textContent = data.weather[0].description;
+  document.querySelector("#weather-description").appendChild(description);
+} 
